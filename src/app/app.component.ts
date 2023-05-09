915e72c8dc7885a8d10b0bc24d735551cbb9b2b3
Daniel Garcia <demonpogp@gmail.com>
2023-05-09 17:49:33 -0500

    Created navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'hd-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'hd-navbar',
+  standalone: true,
+    imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+}
